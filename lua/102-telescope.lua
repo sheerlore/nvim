ce554f340cd0922dfadb16ac881bdbadb49fe11b
uai <forgivemyblue13@gmail.com>
2023-05-14 21:03:14 +0900

    add telescope-file-broweser

--- lua/102-telescope.lua
+++ lua/102-telescope.lua
@@ -1,39 +1,43 @@
 local status, telescope = pcall(require, "telescope")
 if (not status) then return end
+local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
+
+function telescope_buffer_dir()
+  return vim.fn.expand('%:p:h')
+end
 
 telescope.setup {
   defaults = {
-    -- Default configuration for telescope goes here:
-    -- config_key = value,
     mappings = {
-      i = {
-        -- map actions.which_key to <C-h> (default: <C-/>)
-        -- actions.which_key shows the mappings for your picker,
-        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-        ["<C-h>"] = "which_key"
+      n = {
+        ['q'] = actions.close
       }
     }
   },
-  pickers = {
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
-  },
   extensions = {
-    -- Your extension configuration goes here:
-    -- extension_name = {
-    --   extension_config_key = value,
-    -- }
-    -- please take a look at the readme of the extension you want to configure
+    file_browser = {
+      theme = "dropdown",
+      -- disables netrw and use telescope-file-browser in its place
+      hijack_netrw = true,
+      mappings = {
+        ["i"] = {
+        },
+        ["n"] = {
+        }
+      }
+    }
   }
 }
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+telescope.load_extension("file_browser")
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<leader>ff', builtin.find_files, opts)
+-- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>bb', builtin.buffers, opts)
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, opts)
+vim.api.nvim_set_keymap(
+  'n',
+  '<leader>fb',
+  '<cmd>lua require("telescope").extensions.file_browser.file_browser({ path = "%:p:h", select_buffer = true, pwd = telescope_buffer_dir()})<CR>',
+  { noremap = true }
+)
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -36,6 +36,10 @@ return require("packer").startup(function(use)
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
     requires = { 'nvim-lua/plenary.nvim' }
   }
+  use {
+    'nvim-telescope/telescope-file-browser.nvim',
+    requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"}
+  }
 
   -- lsp & server
   use 'neovim/nvim-lspconfig'
